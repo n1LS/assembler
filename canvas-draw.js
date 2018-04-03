7279c473192534b75b320e5c6654b413f20a67cb
nILS Podewski <nils.podewski@datagroup.de>
2018-04-03 08:01:45 +0200

    * fixed scaling and improved performance drastically :-)

--- canvas-draw.js
+++ canvas-draw.js
@@ -1,15 +1,15 @@
-const kWhite        = [0xff,0xff,0xff]
-const kSilver       = [0xbb,0xbb,0xbb]
-const kBlack        = [0x00,0x00,0x00]
-const kGrey         = [0x80,0x80,0x80]
-const kDarkGrey     = [0x40,0x40,0x40]
-const kDarkestGrey  = [0x20,0x20,0x20]
-const kProcess1     = [0xf0,0x60,0x20]
-const kProcess2     = [0x20,0x80,0xf0]
-const kProcessLow1  = [0xa0,0x40,0x20]
-const kProcessLow2  = [0x20,0x60,0xb0]
-const kProcess1Text = [0xff,0xc0,0x80]
-const kProcess2Text = [0x80,0xe0,0xff]
+const kWhite        = 0xffffffff
+const kSilver       = 0xffbbbbbb
+const kBlack        = 0xff000000
+const kGrey         = 0xff808080
+const kDarkGrey     = 0xff404040
+const kDarkestGrey  = 0xff202020
+const kProcess1     = 0xfff06020
+const kProcess2     = 0xff2080f0
+const kProcessLow1  = 0xffa04020
+const kProcessLow2  = 0xff2060b0
+const kProcess1Text = 0xffffc080
+const kProcess2Text = 0xff80e0ff
 
 
 var ctx = null
@@ -20,41 +20,35 @@ var data = null
 var width = 0
 var height = 0
 
-var buffer
-var buffer_image
-var buffer_canvas = null
+var buf
+var buf8
+var buf32
 
 function draw_set_context(canvas, scale) {
     console.log("Setting context")
     ctx = canvas.getContext('2d')
-    width = Math.floor(canvas.width / scale)
-    height = Math.floor(canvas.height / scale)
+    width = row_height * Math.floor(canvas.width / scale / row_height)
+    height = row_height * Math.floor(canvas.height / scale / row_height)
     
-    buffer_image = ctx.getImageData(0, 0, width, height)
+    image_data = ctx.getImageData(0, 0, width, height)
 
-    if (buffer_canvas === null) {
-        buffer_canvas = document.createElement('canvas')
-    }
-
-//    buffer_image = buffer_canvas.getContext('2d').createImageData(width, height)
-    buffer = new Uint8Array(4 * width * height)
+    buf = new ArrayBuffer(image_data.data.length);
+    buf8 = new Uint8ClampedArray(buf);
+    buf32 = new Uint32Array(buf);
 }
 
 function draw_pixel(x, y, color) {
-    var idx = 4 * ((offs_x + x) + (offs_y + y) * width)
+    var idx = ((offs_x + x) + (offs_y + y) * width)
 
-    buffer[idx++] = color[0]
-    buffer[idx++] = color[1]
-    buffer[idx++] = color[2]
-    buffer[idx] = 0xff
+    buf32[idx] = color
 }
 
 function draw_flip() {
-    // ctx.scale(scale, scale)
-    // ctx.drawImage(buffer_canvas, 0, 0, width, height)
-    // ctx.scale(1 / scale, 1 / scale)
-    buffer_image.data.set(buffer)
-    ctx.putImageData(buffer_image, 0, 0)
+    image_data.data.set(buf8)
+    ctx.putImageData(image_data, 0, 0)
+    ctx.scale(scale, scale)
+    ctx.drawImage(ctx.canvas, 0, 0)
+    ctx.scale(1 / scale, 1 / scale)
 }
 
 function draw_rect(x, y, w, h, color) {
