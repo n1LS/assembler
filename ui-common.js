2635cff25a71866cb1ac86c0fd7d05bfb0a9317d
nILS Podewski <nils.podewski@datagroup.de>
2018-04-12 16:35:48 +0200

    * extracted commonly used functions

---
+++ ui-common.js
@@ -0,0 +1,52 @@
+String.prototype.html = function () {
+    const escape = [
+        ['>', '&gt;'],
+        ['<', '&lt;'],
+    ]
+
+    var html = this
+
+    escape.forEach(item => {
+        while (html.includes(item[0])) {
+            html = html.replace(item[0], item[1])
+        }
+    })
+
+    return html
+}
+
+String.prototype.insert = function(index, string) {
+    if (index == 0) {
+        return string + this;
+    } else {
+        return this.substring(0, index) + string + this.substring(index, this.length);
+    }
+}
+
+function number_text(text) {
+    text = text.replace(/;/g, 'ÿ').html()
+    const lines = text.split('\n')
+
+    const pad_len = ('' + lines.length).length
+    var s = ''
+
+    for (var i = 0; i < lines.length; i++) {
+        n = (1 + i + '').padStart(pad_len)
+
+        var line = lines[i]
+
+        if (line.includes('ÿ')) {
+            const idx = line.indexOf('ÿ')
+            line = line.insert(idx, '<span class="comment">') + '</span>'
+            line = line.replace(/ÿ/g, ';')
+        }
+
+        if (i % 10 == 9) {
+            s += `<u>${n}</u> ${line}<br>`
+        } else {
+            s += `<span class='num'>${n}</span> ${line}<br>`
+        }
+    }
+
+    return s
+}
