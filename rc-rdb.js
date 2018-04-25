ce31c568e09742619cdedd620fdee6bdb2d1fcb5
nILS Podewski <nils.podewski@datagroup.de>
2018-04-25 16:23:54 +0200

    + added stub for the rdb debugger

---
+++ rc-rdb.js
@@ -0,0 +1,33 @@
+const rdb_keywords = [
+    'peek',
+    'poke',
+    'step',
+    'run'
+]
+
+// setup ***********************************************************************
+
+create_min_keywords() {
+
+}
+
+create_min_keywords()
+
+// implementation **************************************************************
+
+class Debugger {
+
+    constructor() {
+    }
+
+    attach_to(core) {
+        this.core = core
+    }
+
+    run(instruction) {
+        instruction = instruction.trim()
+
+
+    }
+
+}
